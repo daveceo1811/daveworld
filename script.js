98e0d5a4b36b141fb55bdafd56a845333a972e47
david okonkwo <david070346225@gmail.com>
2022-05-13 09:13:00 +0100

    Dave first world

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('Hi There!');
+Math.random();
+
+let amount = 6;
+let price = 4.99;
+
+let message = 'good nite';
+console.log(message.length);
+
+ 
+console.log('howdy'.length);
+let lateToWork = true;
